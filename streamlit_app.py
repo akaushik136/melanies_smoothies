90103a1c2d5a6fb50103f720d04446780c307ac6
akaushik136 <akaushik@phdata.io>
2024-09-04 15:09:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,6 @@ my_dataframe = session.table("smoothies.public.fruit_options")
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
-pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
-
 # Convert the Snowpark DataFrame to a Pandas DataFrame
 df = my_dataframe.to_pandas()
 
@@ -44,6 +40,10 @@ ingredients_list = st.multiselect(
     fruit_options,
     max_selections = 5
 )
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 if ingredients_list:
     ingredients_string = ''
